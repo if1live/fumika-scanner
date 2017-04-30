bad86d5c8ab555dbe98904d41734ce9d422e6bab
if1live <libsora25@gmail.com>
2017-04-30 20:28:56 +0900

    중고책 가격을 구글 드라이브에 올림

--- service/main.go
+++ service/main.go
@@ -5,6 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/if1live/fumika"
+
+	"net/http"
 
 	"golang.org/x/oauth2/google"
 	spreadsheet "gopkg.in/Iwark/spreadsheet.v2"
@@ -15,7 +21,32 @@ const (
 	fieldTitle     = 1
 	fieldAuthor    = 2
 	fieldPublisher = 3
+	fieldPrice     = 4
+
+	fieldAladinCmd         = 5
+	fieldAladinPriceBest   = fieldAladinCmd + 1
+	fieldAladinPriceGood   = fieldAladinCmd + 2
+	fieldAladinPriceNormal = fieldAladinCmd + 3
+	fieldAladinUpdatedAt   = fieldAladinCmd + 4
+	fieldAladinEnd         = fieldAladinCmd + 5
+
+	fieldYes24Cmd         = fieldAladinEnd
+	fieldYes24PriceBest   = fieldYes24Cmd + 1
+	fieldYes24PriceGood   = fieldYes24Cmd + 2
+	fieldYes24PriceNormal = fieldYes24Cmd + 3
+	fieldYes24UpdatedAt   = fieldYes24Cmd + 4
+	fieldYes24End         = fieldYes24Cmd + 5
+
+	commandSkip = "skip"
 )
+
+type UsedBookFieldList struct {
+	cmd         int
+	priceBest   int
+	priceGood   int
+	priceNormal int
+	updatedAt   int
+}
 
 type Config struct {
 	NaverClientID     string `json:"naver_client_id"`
@@ -39,23 +70,8 @@ func NewConfig(filepath string) Config {
 	return config
 }
 
-func main() {
-	config := NewConfig("config.json")
+func updateInfo(sheet *spreadsheet.Sheet, config Config) {
 	searchAPI := NewSearchAPI(config.NaverClientID, config.NaverClientSecret)
-
-	data, err := ioutil.ReadFile("client_secret.json")
-	checkError(err)
-	conf, err := google.JWTConfigFromJSON(data, spreadsheet.Scope)
-	checkError(err)
-	client := conf.Client(context.TODO())
-
-	service := spreadsheet.NewServiceWithClient(client)
-	spreadsheet, err := service.FetchSpreadsheet(config.SpreadsheetID)
-	checkError(err)
-
-	// get a sheet by the index.
-	sheet, err := spreadsheet.SheetByID(config.SheetID)
-	checkError(err)
 
 	for rowIdx, row := range sheet.Rows {
 		isbn := row[fieldISBN].Value
@@ -74,11 +90,90 @@ func main() {
 		sheet.Update(rowIdx, fieldTitle, item.Title)
 		sheet.Update(rowIdx, fieldAuthor, item.Author)
 		sheet.Update(rowIdx, fieldPublisher, item.Publisher)
-		fmt.Printf("isbn=%s -> title=%s\n", isbn, item.Title)
+		sheet.Update(rowIdx, fieldPrice, item.PriceStr)
+		fmt.Printf("[update info] isbn=%s -> title=%s\n", isbn, item.Title)
 	}
+}
 
+func updateUsedBook(sheet *spreadsheet.Sheet, api fumika.SearchAPI, fields UsedBookFieldList, tag string) {
+	now := time.Now()
+	nowStr := now.Format("2006-01-02 15:04:05")
+
+	for rowIdx, row := range sheet.Rows {
+		cmd := row[fields.cmd].Value
+		if cmd == commandSkip {
+			continue
+		}
+
+		isbn := row[fieldISBN].Value
+		result, err := api.SearchISBN(isbn)
+		if err != nil {
+			continue
+		}
+
+		sheet.Update(rowIdx, fields.cmd, commandSkip)
+		sheet.Update(rowIdx, fields.priceBest, strconv.Itoa(result.PriceBest))
+		sheet.Update(rowIdx, fields.priceGood, strconv.Itoa(result.PriceGood))
+		sheet.Update(rowIdx, fields.priceNormal, strconv.Itoa(result.PriceNormal))
+		sheet.Update(rowIdx, fields.updatedAt, nowStr)
+		fmt.Printf("[update usedbook %s] isbn=%s -> title=%s\n", tag, isbn, result.Title)
+	}
+}
+
+func updateYes24Data(sheet *spreadsheet.Sheet, config Config) {
+	httpclient := &http.Client{}
+	api := fumika.NewYes24(httpclient)
+	fields := UsedBookFieldList{
+		cmd:         fieldYes24Cmd,
+		priceBest:   fieldYes24PriceBest,
+		priceGood:   fieldYes24PriceGood,
+		priceNormal: fieldYes24PriceNormal,
+		updatedAt:   fieldYes24UpdatedAt,
+	}
+	updateUsedBook(sheet, api, fields, "yes24")
+}
+
+func updateAladinData(sheet *spreadsheet.Sheet, config Config) {
+	httpclient := &http.Client{}
+	api := fumika.NewAladin(httpclient)
+
+	fields := UsedBookFieldList{
+		cmd:         fieldAladinCmd,
+		priceBest:   fieldAladinPriceBest,
+		priceGood:   fieldAladinPriceGood,
+		priceNormal: fieldAladinPriceNormal,
+		updatedAt:   fieldAladinUpdatedAt,
+	}
+	updateUsedBook(sheet, api, fields, "aladin")
+}
+
+func main() {
+	config := NewConfig("config.json")
+
+	data, err := ioutil.ReadFile("client_secret.json")
+	checkError(err)
+	conf, err := google.JWTConfigFromJSON(data, spreadsheet.Scope)
+	checkError(err)
+	client := conf.Client(context.TODO())
+
+	service := spreadsheet.NewServiceWithClient(client)
+	spreadsheet, err := service.FetchSpreadsheet(config.SpreadsheetID)
+	checkError(err)
+
+	// get a sheet by the index.
+	sheet, err := spreadsheet.SheetByID(config.SheetID)
+	checkError(err)
+
+	updateInfo(sheet, config)
 	// Make sure call Synchronize to reflect the changes
 	err = sheet.Synchronize()
 	checkError(err)
 
+	updateAladinData(sheet, config)
+	err = sheet.Synchronize()
+	checkError(err)
+
+	updateYes24Data(sheet, config)
+	err = sheet.Synchronize()
+	checkError(err)
 }
